f298f098f1ae3a80e3042decdc7e6d7ca6c8903e
Daniil_Patrushev <iqwerty477@gmail.com>
2023-06-25 11:38:33 +0500

    pre-last-version

--- front/src/components/ready_message.js
+++ front/src/components/ready_message.js
@@ -22,7 +22,7 @@ const TextMessage = ({status, message}) => {
             {message.map((val) => {
                 return(
                     <div className="one_message">
-                        <h1 className="title">[{JSON.stringify(val.timecode).slice(1, -1)}] {JSON.stringify(val.title).slice(1, -1)}</h1>
+                        <h2 className="title">[{JSON.stringify(val.timecode).slice(1, -1)}] {JSON.stringify(val.title).slice(1, -1)}</h2>
                         <p className="text">{JSON.stringify(val.text).slice(1, -1)}</p>
                         <img className='images' src={'data:image/png;base64,' + JSON.stringify(val.image).slice(3, -3)}  alt="PNG MISSED"/>
                     </div>
